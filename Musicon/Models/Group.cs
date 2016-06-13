784401b7a3eef942830ca5e45cd977fce82a55cb
Brian Merchant <btmerchant@comcast.net>
2016-06-12 21:47:54 -0500

    Groups now accesable by menue and listing

--- Musicon/Models/Group.cs
+++ Musicon/Models/Group.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Musicon.Models
 {
     public class Group
     {
         public int GroupId { get; set; }
+
         public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime DateFormed { get; set; }
+
         public string Style { get; set; }
     }
 }
